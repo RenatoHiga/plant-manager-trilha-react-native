ab7460f823691e89f880c980603a164082a652f9
RenatoHiga <renatoka14@gmail.com>
2021-04-25 17:51:36 -0300

    Plant manager finalizado

---
+++ App.tsx
@@ -0,0 +1,54 @@
+import React, { useEffect } from 'react';
+import { Text } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import * as Notifications from 'expo-notifications';
+import { PlantProps } from './src/libs/storage';
+
+import Routes from './src/routes/index';
+import { 
+  useFonts,
+  Jost_400Regular,
+  Jost_600SemiBold,
+} from '@expo-google-fonts/jost';
+
+export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    Jost_400Regular,
+    Jost_600SemiBold,
+  });
+
+  useEffect(() => {
+    const subscription = Notifications.addNotificationReceivedListener(
+      async notification => {
+        const data = notification.request.content.data.plant as PlantProps;
+
+        console.log(data);
+      }
+    );
+
+    return () => subscription.remove();
+
+    // async function notifications() {
+    //   const data = await Notifications.getAllScheduledNotificationsAsync();
+    //   console.log("#######NOTIFICAÇÕES AGENDADAS#######");
+    //   console.log(data);
+
+    //   await Notifications.cancelAllScheduledNotificationsAsync();
+
+    //   const data = await Notifications.getAllScheduledNotificationsAsync();
+    //   console.log("#######NOTIFICAÇÕES AGENDADAS#######");
+    //   console.log(data);
+    // }
+
+    // notifications();
+
+  }, [])
+
+  if (!fontsLoaded)
+    return <AppLoading />;
+
+  return (
+    <Routes />
+  );
+}
